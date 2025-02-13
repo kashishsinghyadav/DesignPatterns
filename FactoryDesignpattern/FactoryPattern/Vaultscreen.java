12f396151b4c460b8855c485bf36b2c8eb68ffd8
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-02-13 20:18:39 +0530

    Update Vaultscreen.java

--- FactoryDesignpattern/FactoryPattern/Vaultscreen.java
+++ FactoryDesignpattern/FactoryPattern/Vaultscreen.java
@@ -1,1 +1,13 @@
+public abstrct class Vaultscreen extends GenricMethods{
 
+public Vaultscreen(Webdriver driver){
+      super(driver);
+
+}
+protected abstract By settingsLocator();
+
+  public void clickOnSettingPage(){
+    click(driver,settingLocator());
+  }
+
+}
