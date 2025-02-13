98c9330200b0f90bec6649cfcfe9670eb08d3fd7
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-02-13 20:15:04 +0530

    Update VaultPagefactory.java

--- FactoryDesignpattern/FactoryPattern/VaultPagefactory.java
+++ FactoryDesignpattern/FactoryPattern/VaultPagefactory.java
@@ -1,1 +1,19 @@
+public class VaultPagefactory{
 
+public static Vaultpage createVaultPagescreen(Webdriver driver){
+          if(os.contain('Mac')){
+              return new MacVaultLocator(driver);
+            
+          }
+            elseif(os.contain('Window')){
+              return new WindowVaultLocator(driver);
+            }
+          else{
+            return null;
+          }
+
+}
+
+
+
+}
