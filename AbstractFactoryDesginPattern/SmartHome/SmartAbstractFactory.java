892b0372d22d415d059823fffa2aa44f34144f62
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-02-18 22:37:51 +0530

    Update SmartAbstractFactory.java

--- AbstractFactoryDesginPattern/SmartHome/SmartAbstractFactory.java
+++ AbstractFactoryDesginPattern/SmartHome/SmartAbstractFactory.java
@@ -1,9 +1,16 @@
 public abstract class SmartAbstractFactory{
   public static SmartAbstractFactory createsmartHome(String companyname){
 
-
+    if(companyname=="Philips")
+      return new PhilipsFactory();
+    else if(companyname=="Xiaomi")
+      return new XiaomiFactory();
+    else
+      return null;
 
   }
+  public SmartFan getfan();
+  public SmartLight getlight();
 
 
 }
