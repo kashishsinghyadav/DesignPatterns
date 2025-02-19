2af4a429b8864ee963890769539fd3029e1cdeb9
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-02-19 22:36:37 +0530

    Update client.java

--- AbstractFactoryDesginPattern/SmartHome/client.java
+++ AbstractFactoryDesginPattern/SmartHome/client.java
@@ -1,6 +1,11 @@
 public class client{
 
 public static void main(String[] args) {
+  SmartAbstractFactory object = SmartAbstractFactory.createsmartHome("Philips");
+  System.out.println(object.getfan());
+  System.out.println(object. getlight());
+  
+ 
   
 
   }
