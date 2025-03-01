754d5ed40c7a51ec138fcb3003e5fa620b750c56
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-03-01 19:56:58 +0530

    Create Zipcompressor.java

---
+++ StrategyDesginPattern/Strategy/Zipcompressor.java
@@ -0,0 +1,8 @@
+public class Zipcompressor implements  Compressor{
+
+    @Override
+    public void compress(){
+     // compress the file
+     System.out.println("zip file compress");
+    } 
+ }
