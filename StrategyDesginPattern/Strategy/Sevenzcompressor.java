31780005de0b969d9459d265e6724be2e65a6aea
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-03-01 19:56:36 +0530

    Create Sevenzcompressor.java

---
+++ StrategyDesginPattern/Strategy/Sevenzcompressor.java
@@ -0,0 +1,8 @@
+public class Sevenzcompressor implements  Compressor{
+
+    @Override
+    public void compress(){
+     // compress the file
+     System.out.println("sevenz file compress");
+    } 
+ }
