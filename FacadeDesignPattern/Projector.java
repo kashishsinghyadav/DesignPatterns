63af3a254a6f9cb96fa7d76ef21e83e3cf2eb395
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-03-09 13:41:33 +0530

    Create Projector.java

---
+++ FacadeDesignPattern/Projector.java
@@ -0,0 +1,9 @@
+public class Projector {
+    public void onProjector(){
+        System.out.println("Projector is on");      
+    }
+    public void offProjector(){
+        System.out.println("Projector is off"); 
+    }
+    
+}
