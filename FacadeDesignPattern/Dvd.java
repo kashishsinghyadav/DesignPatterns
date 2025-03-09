ea27cc139c4bd4f426036ccc17e41b923c61bc4f
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-03-09 13:41:14 +0530

    Create Dvd.java

---
+++ FacadeDesignPattern/Dvd.java
@@ -0,0 +1,11 @@
+public class Dvd {
+
+    public void onDvd(){
+        System.out.println("DVD is playing");
+    }
+    
+    public void offDvd(){
+        System.out.println("DVD is stopped");
+    }
+    
+}
