808fb163b7a08198538a2baa93caf76c7c6397a8
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-03-09 13:41:55 +0530

    Create Light.java

---
+++ FacadeDesignPattern/Light.java
@@ -0,0 +1,9 @@
+public class Light {
+    public void dimLight(){
+        System.out.println("Dimming light");
+    }
+    public void brightenLight(){
+        System.out.println("Brightening light");
+    }
+    
+}
