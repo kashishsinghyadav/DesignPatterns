55d0b0d577749d3460ccaf3c57363ee638fa094a
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-03-30 21:13:15 +0530

    Create Light.java

---
+++ CommandDesignPattern/Light.java
@@ -0,0 +1,14 @@
+public class Light implements  Device{
+
+    @Override
+    public void ondevice() {
+       System.out.println("light is onnnnnnnnnn");
+    }
+
+    @Override
+    public void offdevice() {
+        System.out.println("light is offfffffffffffff");
+    }
+    
+    
+}
