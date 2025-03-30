2c7c0eb4c76ba5c67f22ff62d93ffe963d20baa1
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-03-30 21:12:51 +0530

    Create Fan.java

---
+++ CommandDesignPattern/Fan.java
@@ -0,0 +1,13 @@
+public class Fan implements  Device {
+
+    @Override
+    public void ondevice() {
+        System.out.println("Fan is onnnnnnnnnnnnnnnnn");
+    }
+
+    @Override
+    public void offdevice() {
+        System.out.println("Fan is offffffffff!!!!!");
+    }
+    
+}
