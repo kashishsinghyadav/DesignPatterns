122a8321ae13437aa6757b23fb887264e2211fe6
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-04-05 15:11:28 +0530

    Create ArtCollage.java

---
+++ TemplateDesignPattern/ArtCollage.java
@@ -0,0 +1,6 @@
+public class ArtCollage  extends  RegistrationProcess{
+    @Override
+    public void evaluateCandidate() {
+        System.out.println("the candidate is evaluate on basis of portfolio");
+    }
+}
