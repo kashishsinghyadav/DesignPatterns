322cc877528b9b285f1e219ed181fe356793bcee
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-04-05 15:10:51 +0530

    Create EngineeringCollage.java

---
+++ TemplateDesignPattern/EngineeringCollage.java
@@ -0,0 +1,9 @@
+public class EngineeringCollage  extends RegistrationProcess{
+
+    @Override
+    public void evaluateCandidate() {
+        System.out.println("the candidate is evaluate on basis of jee exam");
+    }
+    
+    
+}
