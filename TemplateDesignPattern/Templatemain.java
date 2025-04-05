3514bfe3911a6f86f1b1412e96489cd9495ae67d
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-04-05 15:10:04 +0530

    Create Templatemain.java

---
+++ TemplateDesignPattern/Templatemain.java
@@ -0,0 +1,10 @@
+
+public class Templatemain {
+    public static void main(String[] args) {
+        RegistrationProcess register = new EngineeringCollage();
+        register.registerStudent();
+        RegistrationProcess register1= new ArtCollage();
+        register1.registerStudent();
+        
+    }
+}
